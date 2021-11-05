f029eb82d6f9650a4cd92b57c3736d87c8da94d2
jasonasante <jason.asante@outlook.com>
2021-11-05 17:38:24 +0000

    Ascii-art

--- ascii-art.go
+++ ascii-art.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	words := os.Args[1]
 	runes := []rune(words)
-	fmt.Println(string(runes))
+	emptySLice := [8][]byte{}
+	for _, char := range runes {
+		content, _ := ioutil.ReadFile(os.Args[2])
+	}
+	// num := 9
+	// matrix := make([][]byte, num)
+	// for i := 0; i < num; i++ {
+	// 	matrix[i] = make([]byte, num)
+	// 	vector := make([]byte, num)
+	// 	for j := 0; j < num; j++ {
+	// 		vector[j] = i*num + j
+	// 		matrix[i] = append(matrix[i], vector[j])
+	// 	}
+	// }
 }
